278b55023a143bee56679bd5b5169c87ed2a5272
m4tty4 <m4tty4.f@gmail.com>
2020-05-11 23:24:14 +0900

    ABC081B AC

---
+++ ABC081B/test_abc081b.py
@@ -0,0 +1,47 @@
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """3
+8 12 40"""
+        output = """2"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """4
+5 6 8 10"""
+        output = """0"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """6
+382253568 723152896 37802240 379425024 404894720 471526144"""
+        output = """8"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
+def resolve():
+    numdivide = 10 ** 9
+    n = int(input())
+    board = list(map(int, input().split(' ')))
+    for i in board:
+        tmpdivide = 0
+        while (i % (2 ** (tmpdivide + 1))) == 0:
+            tmpdivide = tmpdivide + 1
+        if tmpdivide < numdivide:
+            numdivide = tmpdivide
+    print(numdivide)
